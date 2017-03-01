78e8e509f25684c72bb565a6933b33447c9b863f
einargizz <einarog05@ru.is>
2017-03-01 15:19:04 +0000

    Added placeholders for balloon and implemented fart controls (don´t ask)

---
+++ Assets/Scripts/BalloonController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonController : MonoBehaviour {
+
+	SpriteRenderer renderer;
+	Rigidbody2D rigid;
+	Transform transformer;
+
+	public float rotationSpeed, movementspeed;
+
+	public Sprite image1, image2, image3;
+	public bool useFart;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<SpriteRenderer>();
+		rigid = GetComponent<Rigidbody2D>();
+		transformer = GetComponent<Transform>();
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (Input.GetKey(KeyCode.Alpha1)) {
+			renderer.sprite = image1;
+		}
+
+		if (Input.GetKey(KeyCode.Alpha2)) {
+			renderer.sprite = image2;
+		}
+
+		if (Input.GetKey(KeyCode.Alpha3)) {
+			renderer.sprite = image3;
+		}
+
+		if (useFart) {
+			if (Input.GetKey(KeyCode.A)) {
+				rigid.AddTorque(rotationSpeed);
+			}
+			if (Input.GetKey(KeyCode.D)) {
+				rigid.AddTorque(-rotationSpeed);
+			}
+			if (Input.GetKey(KeyCode.W)) {
+				rigid.AddForce(transform.right);
+			}
+		}
+		else {
+		}
+	}
+}
