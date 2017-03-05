ff8f82d1f2e00f017a58cbc6537ba0a34e0e4670
Guðrún Inga Baldursdóttir <gudrunib09@ru.is>
2017-03-05 12:41:06 +0000

    Basic main menu with start and quit button and music

---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UIController : MonoBehaviour {
+
+	//Should be 1 to load Escape scene
+	//Menu needs to be first in build order (0)
+	//Escape second (1)
+	public void LoadScene(int i)
+	{
+		SceneManager.LoadScene (i);
+	}
+	//Call if quitting the application
+	public void Quit()
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit ();
+		#endif
+	}
+}
