d009ee57634e55138f5c2c2166602ec79ea2547e
Ívar Örn Ragnarsson <ivarr15@ru.is>
2017-03-07 20:18:50 +0000

    working camera movement, malfunctioning killbox

---
+++ Assets/KillOnExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillOnExit : MonoBehaviour {
+
+	
+    void OnTriggerExit (Collider other)
+    {
+            Destroy(other.gameObject);
+    }
+}
