e9462d3a03fa6fab1ad7d38251e8a61312d7a757
MagnusMagnusson <magnusm15@ru.is>
2017-03-07 21:54:36 +0000

    Sawblade and golden coins!

---
+++ Assets/Coin_Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin_Rotate : MonoBehaviour {
+	Transform T;
+	public float rotateSpeed;
+	// Use this for initialization
+	void Start () {
+		T = GetComponent<Transform>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		T.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{											
+		if (other.gameObject.CompareTag("Player"))
+		{									  
+			gameObject.SetActive(false);
+		}
+	}
+}
---
+++ Assets/rotate_spike.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate_spike : MonoBehaviour
+{
+	Transform T;
+	public float rotateSpeed;
+	// Use this for initialization
+	void Start()
+	{
+		T = GetComponent<Transform>();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		T.Rotate(0, 0, rotateSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("I am collided");
+
+		Debug.Log("I am become " + other.tag);
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("I am Scary");
+			Destroy(other.gameObject);
+		}
+	}
+}
