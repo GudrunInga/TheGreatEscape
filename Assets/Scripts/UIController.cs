c52fda449dbed39b0bfe4a44da3420995baebfac
Guðrún Inga Baldursdóttir <gudrunib09@ru.is>
2017-03-08 14:40:45 +0000

    Pause menu added to main scene

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,12 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class UIController : MonoBehaviour {
+	
+	public bool paused;
+	public GameObject pauseMenu;
+	public Light spotLight;
+	// Use this for initialization
+	void Start () {
+		paused = false;
+	}
 
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Debug.Log ("Escape");
+			if(paused) {
+				Time.timeScale = 1;
+				spotLight.enabled = true;
+			}
+			else {
+				Time.timeScale = 0;
+				spotLight.enabled = false;
+			}
+			//Debug.Log("Setting TimeScale to " + Time.timeScale);
+			paused = !paused;
+		}
+
+		if (paused) {
+			//Time.timeScale = 0;
+			pauseMenu.SetActive (true);
+		} 
+		else if (!paused && pauseMenu != null) {
+			pauseMenu.SetActive (false);
+		}
+	}
 	//Should be 1 to load Escape scene
 	//Menu needs to be first in build order (0)
 	//Escape second (1)
 	public void LoadScene(int i)
 	{
+		if (paused) {
+			paused = !paused;
+			spotLight.enabled = true;
+			Time.timeScale = 1;
+		}
 		SceneManager.LoadScene (i);
 	}
 	//Call if quitting the application
@@ -19,5 +56,20 @@ public class UIController : MonoBehaviour {
 		#else
 		Application.Quit ();
 		#endif
+	}
+
+	public void UnPause()
+	{
+		paused = !paused;
+		spotLight.enabled = true;
+		Time.timeScale = 1;
+	}
+
+	public void RestartScene()
+	{
+		paused = !paused;
+		spotLight.enabled = true;
+		Time.timeScale = 1;
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
 	}
 }
