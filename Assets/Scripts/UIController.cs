111a6417f961cd73c480bee231525b3fa872c687
Inga <gudrunib09@ru.is>
2017-03-08 20:33:03 -1200

    Started on game over functionality

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class UIController : MonoBehaviour {
+
 	public static UIController instance;
 	void Awake()
 	{
 		instance = this;
 	}
-	public bool paused;
+    //Temporary variable for testing purposes (game over ui)
+    public int tempScore;
+    //GameOverCanvas
+    public GameObject gameOverMenu;
+
+    public bool paused;
+    //PauseMenuCanvas
 	public GameObject pauseMenu;
+    //MainCamera SpotLight
 	public Light spotLight;
+
 	// Use this for initialization
 	void Start () {
 		paused = false;
+        tempScore = 0;
 	}
 
 	// Update is called once per frame
@@ -78,6 +88,8 @@ public class UIController : MonoBehaviour {
 
 	public void GameOver()
 	{
+        spotLight.enabled = false;
+        Time.timeScale = 0;
 
 	}
 }
