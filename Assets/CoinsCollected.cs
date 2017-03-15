00ee95b6f346b2ab2d5033c694f710cd1d212776
Guðrún Inga Baldursdóttir <gudrunib09@ru.is>
2017-03-15 14:58:57 +0000

    Started working on store

---
+++ Assets/CoinsCollected.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinsCollected : MonoBehaviour {
+
+	private Text _coinsCollected;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("I have collected: " + UIController.instance.GetCoins ());
+		_coinsCollected = gameObject.GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_coinsCollected.text = UIController.instance.GetCoins ().ToString();
+	}
+}
