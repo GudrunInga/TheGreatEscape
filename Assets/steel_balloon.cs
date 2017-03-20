b33f72cd1eb0381948e0e1e589fdeb84de4d5127
MagnusMagnusson <magnusm15@ru.is>
2017-03-20 14:55:16 +0000

    steel balloon attatched to player object.

---
+++ Assets/steel_balloon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class steel_balloon : MonoBehaviour {
+	bool active;
+	BalloonController Dad;
+	Rigidbody2D rig;
+	void Start () {
+		Dad = transform.parent.gameObject.GetComponent<BalloonController>();
+		rig = transform.parent.gameObject.GetComponent<Rigidbody2D>();
+	}
+	void Update()
+	{
+		if (active)
+		{
+			Dad.models.Clear();
+			rig.gravityScale = 2;
+		}
+		else
+		{
+			active = true;
+		}
+	}
+}
