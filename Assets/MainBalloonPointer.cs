2e9ab7d23f36b4fc8dd81c5bde61ba236bdec8e9
einargizz <einarog05@ru.is>
2017-03-25 19:06:01 +0000

    Fixed balloon selector.

---
+++ Assets/MainBalloonPointer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainBalloonPointer : MonoBehaviour {
+
+	public Animator menuSelect;
+	int position;
+
+	// Use this for initialization
+	void Start () {
+		position = 3;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && position != 0) {
+			position--;
+			Debug.Log(position);
+		}
+		if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && position != 3) {
+			position++;
+			Debug.Log(position);
+		}
+		menuSelect.ResetTrigger("Start");
+		menuSelect.ResetTrigger("Controls");
+		menuSelect.ResetTrigger("Store");
+		menuSelect.ResetTrigger("Quit");
+		switch (position) {
+			case 3:
+				menuSelect.SetTrigger("Start");
+				break;
+			case 2:
+				menuSelect.SetTrigger("Controls");
+				break;
+			case 1:
+				menuSelect.SetTrigger("Store");
+				break;
+			case 0:
+				menuSelect.SetTrigger("Quit");
+				break;
+		}
+	}
+
+	private void OnDisable() {
+		position = 3;
+	}
+}
--- Assets/SelectOnInput.cs
+++ Assets/SelectOnInput.cs
@@ -6,44 +6,30 @@ public class SelectOnInput : MonoBehaviour {
 
 	public EventSystem eventSystem;
 	public GameObject selectedObject;
-	public GameObject balloonObject;
 
-	//private bool buttonSelected;
-	float speed = 1000.0f;
-	int i;
+
+	private bool buttonSelected;
+
 	// Use this for initialization
-	void Start () {
+	void Start() {
 		eventSystem.SetSelectedGameObject(selectedObject);
-
-		//buttonSelected = true;
-		i = 1;
-		/*if (Input.GetAxisRaw ("Vertical") != 0 && buttonSelected == false) 
-		{
-			eventSystem.SetSelectedGameObject(selectedObject);
-
-			buttonSelected = true;
-		}*/
+		buttonSelected = true;
 	}
 
 	// Update is called once per frame
-	void Update () 
-	{
-		if (balloonObject != null) {
-			if ((Input.GetKeyDown (KeyCode.DownArrow) || Input.GetKeyDown (KeyCode.S)) && i != 3) {
-				var move = new Vector3 (balloonObject.transform.position.x, balloonObject.transform.position.y - 0.9f, balloonObject.transform.position.z);
-				balloonObject.transform.position = move;
-				i++;
-			}
-			if ((Input.GetKeyDown (KeyCode.UpArrow) || Input.GetKeyDown (KeyCode.W)) && i != 1) {
-				var move = new Vector3 (balloonObject.transform.position.x, balloonObject.transform.position.y + 0.9f, balloonObject.transform.position.z);
-				balloonObject.transform.position = move;
-				i--;
-			}
+	void Update() {
+		if (Input.GetAxisRaw("Vertical") != 0 && buttonSelected == false) {
+			eventSystem.SetSelectedGameObject(selectedObject);
+			buttonSelected = true;
 		}
 	}
 
-	/*private void OnDisable()
-	{
+	private void OnEnable() {
+		eventSystem.SetSelectedGameObject(selectedObject);
+		buttonSelected = true;
+	}
+
+	private void OnDisable() {
 		buttonSelected = false;
-	}*/
+	}
 }
