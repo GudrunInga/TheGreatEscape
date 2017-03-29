28633d08d816ad43f38eec514c2209acd7bda81e
einargizz <einarog05@ru.is>
2017-03-29 11:29:21 +0000

    GameOverMenu now has a balloon pointer

---
+++ Assets/Scripts/Animations/GameOverBalloonPointer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverBalloonPointer : MonoBehaviour {
+
+	public Animator gameOverSelect;
+	int position, lastPosition;
+
+	// Use this for initialization
+	void Start() {
+		position = lastPosition = 3;
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && position != 0) {
+			position--;
+			//Debug.Log(position);
+		}
+		if ((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && position != 3) {
+			position++;
+			//Debug.Log(position);
+		}
+
+		if (lastPosition != position) {
+			switch (position) {
+				case 3:
+					AnimTrigger("GRestart");
+					break;
+				case 2:
+					AnimTrigger("GStore");
+					break;
+				case 1:
+					AnimTrigger("GMenu");
+					break;
+				case 0:
+					AnimTrigger("GExit");
+					break;
+			}
+			lastPosition = position;
+		}
+	}
+
+	void AnimTrigger(string triggerName) {
+		foreach (AnimatorControllerParameter p in gameOverSelect.parameters)
+			if (p.type == AnimatorControllerParameterType.Trigger)
+				gameOverSelect.ResetTrigger(p.name);
+		gameOverSelect.SetTrigger(triggerName);
+	}
+
+	private void OnDisable() {
+		position = 3;
+	}
+}
