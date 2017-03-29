08587a49ac0a629a623e1433f45c1b79fb9735d8
MagnusMagnusson <magnusm15@ru.is>
2017-03-29 14:24:23 +0000

    Shield is visible

---
+++ Assets/BalloonShield.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonShield : MonoBehaviour {
+
+	public GameObject Daddy;
+	public GameObject MyShield;
+	private BalloonController BobCat;
+	private bool active;
+	// Use this for initialization
+	void Start () {
+		BobCat = Daddy.GetComponent<BalloonController>();
+		active = BobCat.getShield();
+		if(!active)
+		{
+			MyShield.SetActive(false);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {		  
+		active = true;
+		if (active) { 
+			if (!BobCat.getShield())
+			{	 
+				active = false;		
+				MyShield.SetActive(false);
+			}
+		}
+	}
+}
