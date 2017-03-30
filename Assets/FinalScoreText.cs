620466798626e10b5f9fe31844713cb3941ca7bd
Guðrún Inga Baldursdóttir <gudrunib09@ru.is>
2017-03-30 19:08:46 +0000

    High score

---
+++ Assets/FinalScoreText.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScoreText : MonoBehaviour {
+	public Text text;
+	private HighScoreScrollList _highScoreScrollList;
+
+	public void Setup(HighScore_Manager.ScoreData textLine, HighScoreScrollList highScoreList){
+		//text.text = textLine.coins.ToString ();
+		//text.text += textLine.run.ToString ();
+		//text.text += textLine.score.ToString ();
+		//text.text += textLine.time.ToString ();
+		text.text = textLine.ToString ();
+
+		_highScoreScrollList = highScoreList;
+	}
+}
